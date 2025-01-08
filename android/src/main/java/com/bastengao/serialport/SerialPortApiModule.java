eb4da72fbca3e74a2b17a93ab28224bd6e6cebe5
Chris <Chrisfaustmann@hotmail.com>
2025-01-08 17:29:43 +0200

    Added CSV writing and reading functionality

--- android/src/main/java/com/bastengao/serialport/SerialPortApiModule.java
+++ android/src/main/java/com/bastengao/serialport/SerialPortApiModule.java
@@ -1,21 +1,40 @@
 package com.bastengao.serialport;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.provider.DocumentsContract;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,7 +42,7 @@ public class SerialPortApiModule extends ReactContextBaseJavaModule implements E
     private final ReactApplicationContext reactContext;
     private HandlerThread backgroundThread;
     private Handler backgroundHandler;
-    private volatile boolean keepReading = true;
+    private volatile boolean keepReading = false;
     private static final Pattern JSON_PATTERN = Pattern.compile("\\{(?:[^{}]*|\\{.*?\\})*\\}");
 
     public SerialPortApiModule(final ReactApplicationContext reactContext) {
@@ -39,18 +58,9 @@ public class SerialPortApiModule extends ReactContextBaseJavaModule implements E
         return "SerialPortAPI";
     }
 
-    // @ReactMethod
-    // public void deviceNames(final Callback callback) {
-    //     WritableArray a = Arguments.createArray();
-    //     String[] names = finder.getAllDevices();
-    //     for (String name : names) {
-    //         a.pushString(name);
-    //     }
-    //     callback.invoke(a);
-    // }
-
     @ReactMethod
     public void startReading(final String filePath) {
+        keepReading = true;
         System.out.println("Start Reading...");
         File device = new File(filePath); // Replace with your device file
         if (!device.exists()) {
@@ -93,6 +103,170 @@ public class SerialPortApiModule extends ReactContextBaseJavaModule implements E
         System.out.println("Stopped reading from serial port.");
     }
 
+    @ReactMethod
+    public void writeToCsv(String uri, String fileName, String data, Promise promise) {
+        try {
+            JSONObject jsonObject = new JSONObject(data);
+
+            Iterator<String> keys = jsonObject.keys();
+
+            Uri fileUri = Uri.parse(uri);
+            ContentResolver contentResolver = reactContext.getContentResolver();
+            Uri documentUri = createOrGetDocumentUri(contentResolver, fileUri, fileName);
+
+            if (documentUri == null) {
+                promise.reject("Error creating or resolving file URI");
+                return;
+            }
+
+            boolean isNewFile = isNewFile(contentResolver, documentUri);
+
+            try (OutputStream fos = contentResolver.openOutputStream(documentUri, "wa")) {
+                OutputStreamWriter writer = new OutputStreamWriter(fos);
+                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withRecordSeparator("\n"));
+
+                if (isNewFile) {
+                    while (keys.hasNext()) {
+                        String key = keys.next();
+                        csvPrinter.print(key);
+                    }
+                    csvPrinter.println();
+                }
+
+                keys = jsonObject.keys();
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    csvPrinter.print(jsonObject.get(key));
+                }
+                csvPrinter.println();
+                csvPrinter.flush();
+
+                promise.resolve("CSV written successfully to: " + fileUri.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+                promise.reject("FILE_WRITE_ERROR", "Error writing to file", e);
+            }
+            promise.resolve("File written successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+            promise.reject("Error writing file", e);
+        }
+    }
+
+    @ReactMethod
+    public void readFromCsv(String uri, String fileName, Promise promise) {
+        Uri fileUri = Uri.parse(uri);
+
+        ContentResolver contentResolver = reactContext.getContentResolver();
+        Uri documentUri = createOrGetDocumentUri(contentResolver, fileUri, fileName);
+
+        if (documentUri == null) {
+            promise.reject("Error creating or resolving file URI");
+            return;
+        }
+
+        try (InputStream inputStream = contentResolver.openInputStream(documentUri)) {
+            // Use InputStreamReader for parsing the InputStream
+            InputStreamReader reader = new InputStreamReader(inputStream);
+
+            // Parse the CSV file
+            CSVParser parser = CSVFormat.DEFAULT
+                    .withFirstRecordAsHeader() // Use first row as header
+                    .parse(reader);
+
+            // Create a WritableArray to hold the results
+            WritableArray resultArray = Arguments.createArray();
+
+            // Loop through the records
+            for (CSVRecord record : parser) {
+                WritableMap rowMap = Arguments.createMap();
+
+                // Loop through header names and add each column to the row map
+                for (String header : parser.getHeaderNames()) {
+                    rowMap.putString(header, record.get(header));
+                }
+
+                // Add the row map to the result array
+                resultArray.pushMap(rowMap);
+            }
+
+            // Close the InputStream and Reader
+            reader.close();
+            inputStream.close();
+
+            // Resolve the promise with the result array
+            promise.resolve(resultArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+            promise.reject("FILE_READ_ERROR", "Error reading CSV file", e);
+        }
+    }
+
+    private boolean isNewFile(ContentResolver contentResolver, Uri fileUri) {
+        try (FileInputStream fileInputStream = (FileInputStream) contentResolver.openInputStream(fileUri)) {
+            return fileInputStream == null || fileInputStream.available() == 0;
+        } catch (FileNotFoundException e) {
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Uri createOrGetDocumentUri(ContentResolver contentResolver, Uri treeUri, String fileName) {
+        try {
+            // Build the document URI for the file
+            Uri documentUri = DocumentsContract.buildDocumentUriUsingTree(
+                    treeUri,
+                    DocumentsContract.getTreeDocumentId(treeUri)
+            );
+
+            // Query the directory to see if the file already exists
+            Uri existingFileUri = findExistingFileUri(contentResolver, documentUri, fileName);
+            if (existingFileUri != null) {
+                return existingFileUri; // File already exists
+            }
+
+            // File does not exist, create it
+            return DocumentsContract.createDocument(
+                    contentResolver,
+                    documentUri,
+                    "text/csv", // MIME type for CSV
+                    fileName
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Uri findExistingFileUri(ContentResolver contentResolver, Uri directoryUri, String fileName) {
+        try (Cursor cursor = contentResolver.query(
+                DocumentsContract.buildChildDocumentsUriUsingTree(
+                        directoryUri,
+                        DocumentsContract.getTreeDocumentId(directoryUri)
+                ),
+                new String[]{DocumentsContract.Document.COLUMN_DOCUMENT_ID, DocumentsContract.Document.COLUMN_DISPLAY_NAME},
+                null,
+                null,
+                null
+        )) {
+            if (cursor != null) {
+                while (cursor.moveToNext()) {
+                    String existingFileName = cursor.getString(cursor.getColumnIndexOrThrow(DocumentsContract.Document.COLUMN_DISPLAY_NAME));
+                    String documentId = cursor.getString(cursor.getColumnIndexOrThrow(DocumentsContract.Document.COLUMN_DOCUMENT_ID));
+
+                    // Perform a strict match on file name
+                    if (fileName.equals(existingFileName)) {
+                        return DocumentsContract.buildDocumentUriUsingTree(directoryUri, documentId);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     private String cleanData(String input) {
         Matcher matcher = JSON_PATTERN.matcher(input);
 
@@ -106,97 +280,6 @@ public class SerialPortApiModule extends ReactContextBaseJavaModule implements E
         // Return the valid payload (the last JSON object found)
         return validPayload;
     }
-
-    // @ReactMethod
-    // public void devicePaths(final Callback callback) {
-    //     WritableArray p = Arguments.createArray();
-    //     String[] paths = finder.getAllDevicesPath();
-    //     for (String path : paths) {
-    //         p.pushString(path);
-    //     }    
-    //     callback.invoke(p);
-    // }
-
-//    @ReactMethod
-//    public void setSuPath() {
-//        try {
-//            Process process = Runtime.getRuntime().exec("/system/xbin/su");
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
-    // @ReactMethod
-    // public void getSuPath(final Callback callback) {
-    //     callback.invoke(SerialPort.getSuPath());
-    // }
-
-    // @ReactMethod
-    // public void open(final String path, int baudRate, int parity, int dataBits, int stopBits, int readBufferSize, Promise promise) {
-    //     if (serialPorts.containsKey(path)) {
-    //         promise.resolve(serialPorts.get(path).toJS());
-    //         return;
-    //     }
-
-    //     Remover remover = new Remover() {
-    //         @Override
-    //         public void remove() {
-    //             serialPorts.remove(path);
-    //         }
-    //     };
-
-    //     try {
-    //         SerialPort serialPort = SerialPort.newBuilder(path, baudRate)
-    //                 .parity(parity)
-    //                 .dataBits(dataBits)
-    //                 .stopBits(stopBits)
-    //                 .build();
-
-    //         SerialPortWrapper wrapper = new SerialPortWrapper(path, readBufferSize, serialPort, this, remover);
-
-    //         // TODO: handle previous value
-    //         serialPorts.put(path, wrapper);
-    //         promise.resolve(wrapper.toJS());
-    //     } catch (IOException e) {
-    //         System.out.println("error: " + e.getMessage());
-    //         e.printStackTrace();
-    //         promise.reject(null, e.getMessage());
-    //     } catch (SecurityException e) {
-    //         System.out.println("error: " + e.getMessage());
-    //         e.printStackTrace();
-    //         promise.reject(null, "no permission to read or write this serial port");
-    //     }
-    // }
-
-    // @ReactMethod
-    // public void send(String path, String hexStr, Promise promise) {
-    //     SerialPortWrapper wrapper = serialPorts.get(path);
-    //     if (wrapper == null) {
-    //         promise.reject(null, "serialport not open");
-    //     }
-
-    //     byte[] buffer = hexStringToByteArray(hexStr);
-    //     try {
-    //         wrapper.write(buffer);
-    //         promise.resolve(true);
-    //         Log.i("serialport", "send: " + hexStr);
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //         promise.reject(null, e.getMessage());
-    //     }
-    // }
-
-    // @ReactMethod
-    // public void close(String path, Promise promise) {
-    //     SerialPortWrapper wrapper = serialPorts.get(path);
-    //     if (wrapper == null) {
-    //         promise.reject(null, "serialport not open");
-    //     }
-
-    //     wrapper.close();
-    //     promise.resolve(true);
-    // }
-
     public void sendEvent(final String eventName, final WritableMap event) {
         reactContext.runOnUiQueueThread(new Runnable() {
             @Override
@@ -206,26 +289,4 @@ public class SerialPortApiModule extends ReactContextBaseJavaModule implements E
             }
         });
     }
-
-    // public static byte[] hexStringToByteArray(String s) {
-    //     int len = s.length();
-    //     byte[] data = new byte[len / 2];
-    //     for (int i = 0; i < len; i += 2) {
-    //         data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-    //                 + Character.digit(s.charAt(i + 1), 16));
-    //     }
-    //     return data;
-    // }
-
-    // private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-
-    // public static String bytesToHex(byte[] bytes, int length) {
-    //     char[] hexChars = new char[length * 2];
-    //     for (int j = 0; j < length; j++) {
-    //         int v = bytes[j] & 0xFF;
-    //         hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-    //         hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-    //     }
-    //     return new String(hexChars);
-    // }
 }
--- src/API.ts
+++ src/API.ts
@@ -4,37 +4,8 @@ const { SerialPortAPI } = NativeModules;
 const eventEmitter = Platform.OS === 'android' ? new NativeEventEmitter(SerialPortAPI) : null;
 
 export default class API {
-  /**
-   * @callback stringArrayCallback
-   * @param {Array<string>} values
-   */
-
-  /**
-   * @callback stringCallback
-   * @param {string} value
-   */
-
-
-  /**
-   * @typedef {Object} openOptions
-   * @property {number} baudRate such as 9600
-   * @property {number} [parity=0] 0: none, 1: odd, 2: even
-   * @property {number} [dataBits=8] 5~8
-   * @property {number} [stopBits=1] 1 or 2
-   * @property {number} [readBufferSize=64] set read buffer size
-   */
-
-  /**
-   * Get serial port device names
-   * @param {stringArrayCallback} callback
-   */
-  // static deviceNames(callback: (names: string[]) => void) {
-  //   if (Platform.OS !== 'android') throw new Error(`Not support ${Platform.OS}`)
-  //   SerialPortAPI.deviceNames(callback);
-  // }
-
   static startReading(filePath: string) {
-      console.log(filePath);
+      console.log(`Start reading from ${filePath}...`);
       SerialPortAPI.startReading(filePath);
   };
 
@@ -43,59 +14,28 @@ export default class API {
     SerialPortAPI.stopReading();
   }
 
+  static async writeToCsv(uri: string, fileName: string, data: string) {
+    console.log("Writing to file...");
+    await SerialPortAPI.writeToCsv(uri, fileName, data)
+    console.log("Successfully written to file...");
+  }
+
+  static async readFromCsv(uri: string, fileName: string) {
+    console.log("Reading from CSV...");
+    const data = await SerialPortAPI.readFromCsv(uri, fileName);
+    console.log("Successfully read from CSV...");
+    return data;
+  }
+
   static onDataReceived(listener: (data: any) => void) {
     return eventEmitter?.addListener('onDataReceived', listener);
   }
 
-  /**
-   * Get serial port device paths
-   * @param {stringArrayCallback} callback
-   */
-  // static devicePaths(callback: (paths: string[]) => void) {
-  //   if (Platform.OS !== 'android') throw new Error(`Not support ${Platform.OS}`)
-  //   SerialPortAPI.devicePaths(callback);
-  // }
+  static removeListener(listener: (data: any) => void) {
+    return eventEmitter?.removeListener('onDataReceived', listener);
+  }
 
-  /**
-   * Get serial port device paths
-   * @returns {Promise<Array<string>>} paths
-   */
-  // static devicePathsAsync() {
-  //   if (Platform.OS !== 'android') throw new Error(`Not support ${Platform.OS}`)
-  //   return new Promise((resolve) => {
-  //     SerialPortAPI.devicePaths(resolve);
-  //   });
-  // }
-
-  /**
-   * set su binary path
-   * @param {string} suPath
-   */
-  // static setSuPath() {
-  //   if (Platform.OS !== 'android') throw new Error(`Not support ${Platform.OS}`)
-  //   SerialPortAPI.setSuPath();
-  // }
-
-  /**
-   * get su binary path
-   * @param {stringCallback} callback
-   */
-  // static getSuPath(callback: (suPath: string) => void) {
-  //   if (Platform.OS !== 'android') throw new Error(`Not support ${Platform.OS}`)
-  //   SerialPortAPI.getSuPath(callback)
-  // }
-
-  /**
-   * Open serial port
-   * @param {string} devicePath device path
-   * @param {openOptions} options
-   * @returns {Promise<SerialPort>} connected serial port
-   */
-  // static open(devicePath: string, {baudRate, parity = 0, dataBits = 8, stopBits = 1, readBufferSize = 64}: { baudRate: number, parity?: number, dataBits?: number, stopBits?: number, readBufferSize?: number }): Promise<SerialPort> {
-  //   if (Platform.OS !== 'android') throw new Error(`Not support ${Platform.OS}`)
-  //   return SerialPortAPI.open(devicePath, baudRate, parity, dataBits, stopBits, readBufferSize)
-  //     .then((serialPort: SerialPortWrapper) => {
-  //       return Promise.resolve(new SerialPort(serialPort, eventEmitter!));
-  //     })
-  // }
+  static removeAllListeners() {
+    return eventEmitter?.removeAllListeners('onDataReceived');
+  }
 }
